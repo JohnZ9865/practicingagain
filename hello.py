329c5feaf552542a7e7e7e0ee17921c1eee00e79
JohnZ9865 <zhou9865@gmail.com>
2020-05-01 18:17:51 -0700

    wrote prime number searcher in hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
-print("Hello Wrold")
+def pns(a, b): #prime numer searcher
+  primeNumbers = []
+  for i in range(a, b + 1):
+    isPrimeFlag = True
+    for loop in range(2, i):
+      if i % loop == 0:
+        isPrimeFlag = False
+        break
+    if isPrimeFlag == True:
+      primeNumbers.append(i)
+  print(primeNumbers)
+
+i0 = int(input('Please enter the first integer: '))
+i1 = int(input('Please enter the second integer: '))
+pns(i0, i1)
+    
